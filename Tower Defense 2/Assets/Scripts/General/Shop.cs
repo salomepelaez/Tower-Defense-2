c075645baa9e4586cce9b6c0e1d657ae870ace30
Salomé Pelaez <spsalome@gmail.com>
2019-12-05 16:47:46 -0500

    First steps of the shop

--- Tower Defense 2/Assets/Scripts/General/Shop.cs
+++ Tower Defense 2/Assets/Scripts/General/Shop.cs
@@ -1,31 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Shop : MonoBehaviour
 {
     BuildManager buildManager;
+        
+    private int basicTurretPrice = 50;
+    private int doubleShooterPrice = 100;
+    private int laserTurret = 150;
+
+    public TextMeshProUGUI noMoney;
+
+    private void Awake()
+    {
+        noMoney = GameObject.Find("InsufficientMoney").GetComponent<TextMeshProUGUI>();
+    }
 
     private void Start()
     {
         buildManager = BuildManager.instance;
+        ShopManager.money = 100;
     }
-
+       
     public void PurchaseBasicTurret()
     {
-        Debug.Log("buñuelo");
-        buildManager.SetTurret(buildManager.basicTurret);
+        if (ShopManager.money >= basicTurretPrice)
+        {
+            buildManager.SetTurret(buildManager.basicTurret);
+            ShopManager.money = ShopManager.money - basicTurretPrice;
+        }
+
+        else if(ShopManager.money < basicTurretPrice)
+        {
+            StartCoroutine(InsufficientMoney());
+        }
     }
 
     public void PurchaseDoubleShooterTurret()
     {
-        Debug.Log("macaco");
-        buildManager.SetTurret(buildManager.doubleShooterTurret);
+        if (ShopManager.money >= doubleShooterPrice)
+        {
+            buildManager.SetTurret(buildManager.doubleShooterTurret);
+            ShopManager.money = ShopManager.money - doubleShooterPrice;
+        }
+
+        else if (ShopManager.money < doubleShooterPrice)
+        {
+            StartCoroutine(InsufficientMoney());
+        }
     }
 
     public void PurchaseLaserTurret()
     {
-        Debug.Log("piu");
-        buildManager.SetTurret(buildManager.laserTurret);
+        if (ShopManager.money >= laserTurret)
+        {
+            buildManager.SetTurret(buildManager.laserTurret);
+            ShopManager.money = ShopManager.money - laserTurret;
+        }
+
+        else if (ShopManager.money < laserTurret)
+        {
+            StartCoroutine(InsufficientMoney());
+        }
+    }
+
+    IEnumerator InsufficientMoney()
+    {
+        noMoney.text = "You don't have enough money";
+
+        yield return new WaitForSeconds(2f);
+
+        noMoney.text = "";
     }
 }
---
+++ Tower Defense 2/Assets/Scripts/General/ShopManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShopManager : MonoBehaviour
+{
+    public TextMeshProUGUI moneyCounter;
+
+    public static int money;
+       
+    private void Update()
+    {
+        moneyCounter.text = "= " + money;
+    }
+}
